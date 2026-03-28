3cd80534253f0ca14222b8f1c0ed581d42f948ce
SergRikhter <enigmat0907@gmail.com>
2026-03-28 18:18:40 +0200

    Move plugin info (name, author, description) to separate header

---
+++ aimp-discord-presence-ng/src/headers/AppInfo.h
@@ -0,0 +1,9 @@
+#pragma once
+namespace AppInfo
+{
+	static const wchar_t* PluginName = L"AIMP Discord Presence NG";
+	static const wchar_t* Author = L"Sergio Zhyltsov";
+	static const wchar_t* Contact = L"My Discord: sergey.rikhter";
+	static const wchar_t* Description = L"AIMP plugin for displaying current track in Discord Rich Presence.";
+}
+
--- aimp-discord-presence-ng/src/plugin/AImpPlugin.cpp
+++ aimp-discord-presence-ng/src/plugin/AImpPlugin.cpp
@@ -1,21 +1,17 @@
 #include "pch.h" 
 #include "AimpPlugin.h"
-#include <string>
-
-static const wchar_t* Name = L"AIMP Discord Presence NG";
-static const wchar_t* Author = L"Mr.Secret";
-static const wchar_t* Description = L"AIMP plugin for displaying current track in Discord Rich Presence.";
+#include "AppInfo.h"
 
 TChar* WINAPI AimpPlugin::InfoGet(int Index) {
 
     switch (Index)
     {
       case AIMP_PLUGIN_INFO_NAME:
-        return const_cast<PWCHAR>(Name);
+        return const_cast<PWCHAR>(AppInfo::PluginName);
       case AIMP_PLUGIN_INFO_AUTHOR:
-        return const_cast<PWCHAR>(Author);
+        return const_cast<PWCHAR>(AppInfo::Author);
       case AIMP_PLUGIN_INFO_SHORT_DESCRIPTION:
-        return const_cast<PWCHAR>(Description);
+        return const_cast<PWCHAR>(AppInfo::Description);
      break;
     }
     return 0;
